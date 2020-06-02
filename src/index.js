5549ebe43d314df7abd22aaa62ba4a8891c1a22f
koodoodo <jarrod.lloyd.14@gmail.com>
2020-06-02 22:56:23 +1000

    Error Handling

--- src/index.js
+++ src/index.js
@@ -24,41 +24,48 @@ calculate();
 
 async function calculate()
 {
-  const calls = await getObject(coviuBucketName, callsPath);
-  const teams = await getObject(coviuBucketName, teamsPath);
-  const users = await getObject(coviuBucketName, usersPath);
+  try
+  {
+    const calls = await getObject(coviuBucketName, callsPath);
+    const teams = await getObject(coviuBucketName, teamsPath);
+    const users = await getObject(coviuBucketName, usersPath);
 
-  const usersInCalls = calls.reduce(function (counter, item) {
-    item.participants.forEach(participant => {
-      const user_id = participant.user_id;
-      counter[user_id] = counter.hasOwnProperty(user_id) ? counter[user_id] + 1 : 1; 
-    });
-    return counter;
-  }, {});
+    const usersInCalls = calls.reduce(function (counter, item) {
+      item.participants.forEach(participant => {
+        const user_id = participant.user_id;
+        counter[user_id] = counter.hasOwnProperty(user_id) ? counter[user_id] + 1 : 1; 
+      });
+      return counter;
+    }, {});
 
-  const userWithMostCalls = users[Object.keys(usersInCalls).reduce((a, b) => usersInCalls[a] > usersInCalls[b] ? a : b)];
-  console.log(`User with most calls is ${userWithMostCalls.first_name} ${userWithMostCalls.last_name}`);
-  
-  const teamsInCalls = calls.filter(call => call.started_at >= '2019-03-01T00:00:00-00:00' && call.started_at < '2019-04-01T00:00:00-00:00')
-  .reduce(function (counter, item) {
-    const p = item.team_id;
-    counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
-    return counter;
-  }, {});
+    const userWithMostCalls = users[Object.keys(usersInCalls).reduce((a, b) => usersInCalls[a] > usersInCalls[b] ? a : b)];
+    console.log(`User with most calls is ${userWithMostCalls.first_name} ${userWithMostCalls.last_name}`);
+    
+    const teamsInCalls = calls.filter(call => call.started_at >= '2019-03-01T00:00:00-00:00' && call.started_at < '2019-04-01T00:00:00-00:00')
+    .reduce(function (counter, item) {
+      const p = item.team_id;
+      counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+      return counter;
+    }, {});
 
-  const teamWithLeastCalls = teams[Object.keys(teamsInCalls).reduce((a, b) => teamsInCalls[a] < teamsInCalls[b] ? a : b)];
-  console.log(`Team with least calls in March 2019 is ${teamWithLeastCalls.name}`);
+    const teamWithLeastCalls = teams[Object.keys(teamsInCalls).reduce((a, b) => teamsInCalls[a] < teamsInCalls[b] ? a : b)];
+    console.log(`Team with least calls in March 2019 is ${teamWithLeastCalls.name}`);
 
-  const usersInShortCalls = calls.filter(call => call.duration < 120000).reduce(function (counter, item) {
-    item.participants.forEach(participant => {
-      const user_id = participant.user_id;
-      counter[user_id] = counter.hasOwnProperty(user_id) ? counter[user_id] + 1 : 1; 
-    });
-    return counter;
-  }, {});
+    const usersInShortCalls = calls.filter(call => call.duration < 120000).reduce(function (counter, item) {
+      item.participants.forEach(participant => {
+        const user_id = participant.user_id;
+        counter[user_id] = counter.hasOwnProperty(user_id) ? counter[user_id] + 1 : 1; 
+      });
+      return counter;
+    }, {});
 
-  const userWithMostShortCalls = users[Object.keys(usersInShortCalls).reduce((a, b) => usersInShortCalls[a] > usersInShortCalls[b] ? a : b)];
-  console.log(`User with most short calls is ${userWithMostShortCalls.first_name} ${userWithMostShortCalls.last_name}`);
+    const userWithMostShortCalls = users[Object.keys(usersInShortCalls).reduce((a, b) => usersInShortCalls[a] > usersInShortCalls[b] ? a : b)];
+    console.log(`User with most short calls is ${userWithMostShortCalls.first_name} ${userWithMostShortCalls.last_name}`);
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
 }
 
 async function getObject (bucket, filePath) 

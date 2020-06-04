c9a5bfcb984aa32945ea98a6a8fa8d24f6dae6dc
koodoodo <jarrod.lloyd.14@gmail.com>
2020-06-05 09:34:02 +1000

    Refactor

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const calculate = require("./calculate");
     data = await getData();
     if(data)
     {
-      const userWithMostCalls = await calculate.calculateUsersWithMostCalls(calls, users);
+      const userWithMostCalls = await calculate.calculateUsersWithMostCalls(data.calls, data.users);
       console.log(`User with most calls is ${userWithMostCalls.first_name} ${userWithMostCalls.last_name}`);
       
-      const teamWithLeastCalls = await calculate.calculateTeamWithLeastCalls(calls, teams);    
+      const teamWithLeastCalls = await calculate.calculateTeamWithLeastCalls(data.calls, data.teams);    
       console.log(`Team with least calls in March 2019 is ${teamWithLeastCalls.name}`);
 
-      const userWithMostShortCalls = await calculate.calculateUsersInShortCalls(calls, users);
+      const userWithMostShortCalls = await calculate.calculateUsersInShortCalls(data.calls, data.users);
       console.log(`User with most short calls is ${userWithMostShortCalls.first_name} ${userWithMostShortCalls.last_name}`);
     }
   }
